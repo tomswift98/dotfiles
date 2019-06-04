db45c926dd7d3ad4f223dcdad173442ebeed58bc
tomswift98 <liam.ginger.bennett@gmail.com>
2019-06-04 18:46:35 -0500

    various updates

--- .config/i3/lock.sh
+++ .config/i3/lock.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-ICON=$HOME/Pictures/lock.jpg
+icon=~/.config/i3/lock.jpg
 tmpbg=/tmp/screen.png
 scrot /tmp/screen.png
 convert convert -scale 2% -scale 5000% $tmpbg $tmpbg
-convert $tmpbg $ICON -gravity center -composite -matte $tmpbg
+convert $tmpbg $icon -gravity center -composite -matte $tmpbg
 i3lock -u -i $tmpbg
