cfaf798bd014cfc53804c2048926281e10e31762
tomswift98 <liam.ginger.bennett@gmail.com>
2019-06-07 20:31:09 -0500

    vim and firefox updates

--- .config/i3/lock.sh
+++ .config/i3/lock.sh
@@ -2,6 +2,6 @@
 icon=~/.config/i3/lock.jpg
 tmpbg=/tmp/screen.png
 scrot /tmp/screen.png
-convert convert -scale 2% -scale 5000% $tmpbg $tmpbg
+convert convert -scale 2% -scale 5050% $tmpbg $tmpbg
 convert $tmpbg $icon -gravity center -composite -matte $tmpbg
 i3lock -u -i $tmpbg & sleep 5 && xset s activate
